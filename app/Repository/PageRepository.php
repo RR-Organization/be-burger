1749d0e4b1c8425d6054958429db2341a0f56c5b
rizki <muhammadrizkitkj123@gmail.com>
2023-08-18 15:00:09 +0800

    create page repository

--- app/Repository/PageRepository.php
+++ app/Repository/PageRepository.php
@@ -7,6 +7,8 @@ use App\Models\PageModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
+use function PHPUnit\Framework\isEmpty;
+
 class PageRepository implements PageInterfaces
 {
     protected $model;
@@ -28,12 +30,12 @@ class PageRepository implements PageInterfaces
 
     public function createData(Request $request)
     {
-        $validation = Validator::make($request->all(),[
+        $validation = Validator::make($request->all(), [
             'description' => 'required'
         ]);
 
         if ($validation->fails()) {
-            return $validation;  
+            return $validation;
         }
 
         $status = $this->model->where('status', true)->first();
@@ -43,6 +45,33 @@ class PageRepository implements PageInterfaces
 
         try {
             $data = new $this->model;
+            $data->description = $request->input('description');
+            $data->status = true;
+            $data->save();
+
+            return $data;
+        } catch (\Throwable $th) {
+            return $th;
+        }
+    }
+
+    public function updateData(Request $request, $id)
+    {
+
+        $data = $this->model->where('id', $id)->first();
+        if (!$data) {
+           return ('id-notfound');
+        }
+
+        $validation = Validator::make($request->all(), [
+            'description' => 'required'
+        ]);
+
+        if ($validation->fails()) {
+            return $validation;
+        }
+
+        try {
             $data->description = $request->input('description');
             $data->status = true;
             $data->save();

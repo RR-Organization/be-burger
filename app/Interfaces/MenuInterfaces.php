ac1e9f69fdc2d08e493c84d1082c1d8a2c76fed1
rizki <muhammadrizkitkj123@gmail.com>
2023-08-21 17:46:05 +0800

    create menu interfaces

---
+++ app/Interfaces/MenuInterfaces.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace App\Interfaces;
+
+use Illuminate\Http\Request;
+
+interface MenuInterfaces
+{
+    public function getAllData();
+    public function createData(Request $request);
+}
